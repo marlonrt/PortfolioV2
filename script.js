c9d06d8a3a1838bf4719a10d47a4a3c921241f04
Marlon <repartame@gmail.com>
2023-05-21 23:57:16 -0300

    Minor bug fixes

---
+++ script.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+  function loadContent(url, targetId) {
+    $("#" + targetId).hide();
+    $.get(url, function (data) {
+      $("#" + targetId).html(data).fadeIn(function () {
+        $(".navbar-collapse").collapse("hide");
+      });
+    });
+  }
+
+  loadContent("html/sobre.html", "content");
+
+  $(document).on("click", ".navbar-nav .nav-link", function (e) {
+    e.preventDefault();
+    var page = $(this).data("page");
+    loadContent(page, "content");
+  });
+
+  $(document).on("click", function (e) {
+    if (!$(e.target).hasClass("navbar-toggler")) {
+      $(".navbar-collapse").collapse("hide");
+    }
+  });
+});
