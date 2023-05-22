c59be278e233e00b1ac93a5bfcf2814108c7b505
Marlon <repartame@gmail.com>
2023-05-22 00:41:46 -0300

    Fixed carousel autoplaySpeed

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
-$(document).ready(function () {
+$(document).ready(function() {
   function loadContent(url, targetId) {
     $("#" + targetId).hide();
-    $.get(url, function (data) {
-      $("#" + targetId).html(data).fadeIn(function () {
-        $(".navbar-collapse").collapse("hide");
+    $.get(url, function(data) {
+      $("#" + targetId).html(data).fadeIn(function() {
+        $(".navbar-collapse").removeClass("show").addClass("hide");
       });
     });
   }
 
   loadContent("html/sobre.html", "content");
 
-  $(document).on("click", ".navbar-nav .nav-link", function (e) {
+  $(document).on("click", ".navbar-nav .nav-link", function(e) {
     e.preventDefault();
     var page = $(this).data("page");
     loadContent(page, "content");
   });
 
-  $(document).on("click", function (e) {
+  $(document).on("click", function(e) {
     if (!$(e.target).hasClass("navbar-toggler")) {
-      $(".navbar-collapse").collapse("hide");
+      $(".navbar-collapse").removeClass("show").addClass("hide");
     }
   });
 });
--- script/projetos.js
+++ script/projetos.js
@@ -1,12 +1,20 @@
 $(document).ready(function () {
     $(".owl-carousel").owlCarousel({
-        items:1,
-        loop:true,
-        nav:true,
-        dots:true,
-        autoplay:true,
-        autoplaySpeed:1000,
-        smartSpeed:1500,
-        autoplayHoverPause:true
+        items: 1,
+        loop: true,
+        nav: true,
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        smartSpeed: 1500,
+        autoplayHoverPause: true
+    });
+
+    $(".owl-carousel").on("translated.owl.carousel", function (event) {
+        var carousel = $(event.target);
+        carousel.trigger("stop.owl.autoplay");
+        setTimeout(function () {
+            carousel.trigger("play.owl.autoplay", [5000]);
+        }, 5000);
     });
 });
